ef5e3c8e83fd8a70bad6e86f912015d68f3c1866
PiorBR1 <160504825+PiorBR1@users.noreply.github.com>
2024-11-11 12:11:13 -0300

    carga_Eject

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Raul!</Text>
+      <Text>Pior ne felas</Text>
       <StatusBar style="auto" />
     </View>
   );
